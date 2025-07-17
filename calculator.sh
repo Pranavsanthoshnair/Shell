654cab87233b6e14e1dcf717c160d80a2ef1c049
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-07-17 19:48:38 +0530

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+
+echo "Addition: $((a + b))"
+echo "Subtraction: $((a - b))"
+echo "Multiplication: $((a * b))"
+echo "Division: $((a / b))"
